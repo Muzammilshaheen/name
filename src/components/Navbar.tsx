72d2652ea35071537591e6321fc3171777974638
muzammil shaheen <muzammilshaheen77@gmail.com>
2024-11-18 06:44:25 +0500

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,35 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 import { LuMenu } from "react-icons/lu";
-
 
 const Navbar = () => {
   return (
-    <div className='container pt-8'>
-      <div className='flex justify-between items-center'>
-
-      <div className='text-xl font-medium'>
-          <img src='/ms.png' alt='Logo' className='inline w-28 h-20 mr-2 rounded-full' />
-          <div className='text-red-900 text-sm'>Muzammil Shaheen</div>
+    <div className="container pt-8">
+      <div className="flex justify-between items-center">
+        <div className="text-xl font-medium">
+          <Image
+            src="/ms.png"
+            alt="Logo"
+            width={112} 
+            height={80} 
+            className="inline rounded-full"
+            priority 
+          />
+          <div className="text-red-900 text-sm">Muzammil Shaheen</div>
         </div>
 
-         <ul className='gap-10 lg:gap-16 md:flex text-2xl inline-block mr-10'>
-          <li className='menuLink'><a href="#hero">Home</a></li>
-          <li className='menuLink'><a href="#about">About</a></li>
-          <li className='menuLink'><a href="#projects">Projects</a></li>
-          <li className='menuLink'><a href="#skill">Skills</a></li>
-          <li className='menuLink'><a href="#contact">Contact</a></li>
-         </ul>
+        <ul className="gap-10 lg:gap-16 md:flex text-2xl inline-block mr-10">
+          <li className="menuLink"> <a href="#hero">Home</a> </li>
+          <li className="menuLink"><a href="#about">About</a></li>
+          <li className="menuLink"><a href="#projects">Projects</a></li>
+          <li className="menuLink"><a href="#skill">Skills</a></li>
+          <li className="menuLink"><a href="#contact">Contact</a></li>
+        </ul>
 
-         <LuMenu className='md:hidden' size={30} />
-
-
+        <LuMenu className="md:hidden" size={30} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
